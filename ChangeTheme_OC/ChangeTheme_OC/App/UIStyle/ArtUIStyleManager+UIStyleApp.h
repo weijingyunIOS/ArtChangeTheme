f49aa0a4c13b1995a5f0f98547dd8cc7331f60ce
weijingyun <weijingyun@weijingyundeMacBook-Air.local>
2017-08-24 11:24:31 +0800

    添加全局的

---
+++ ChangeTheme_OC/ChangeTheme_OC/App/UIStyle/ArtUIStyleManager+UIStyleApp.h
@@ -0,0 +1,15 @@
+//
+//  ArtUIStyleManager+UIStyleApp.h
+//  ChangeTheme_OC
+//
+//  Created by weijingyun on 2017/8/24.
+//  Copyright © 2017年 weijingyun. All rights reserved.
+//
+
+#import "ArtUIStyleManager.h"
+
+@interface ArtUIStyleManager (UIStyleApp)
+
+- (NSString *)getStyleName_App;
+
+@end
---
+++ ChangeTheme_OC/ChangeTheme_OC/App/UIStyle/ArtUIStyleManager+UIStyleApp.m
@@ -0,0 +1,17 @@
+//
+//  ArtUIStyleManager+UIStyleApp.m
+//  ChangeTheme_OC
+//
+//  Created by weijingyun on 2017/8/24.
+//  Copyright © 2017年 weijingyun. All rights reserved.
+//
+
+#import "ArtUIStyleManager+UIStyleApp.h"
+
+@implementation ArtUIStyleManager (UIStyleApp)
+
+- (NSString *)getStyleName_App {
+    return @"AppStyle.plist";
+}
+
+@end
