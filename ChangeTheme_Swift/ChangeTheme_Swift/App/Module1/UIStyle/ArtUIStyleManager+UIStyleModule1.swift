0f5a0811151c7bcfc6cc79189d271febadfbd229
weijingyun <weijingyun@weijingyundeMacBook-Air.local>
2017-09-04 18:05:34 +0800

    扩展

---
+++ ChangeTheme_Swift/ChangeTheme_Swift/App/Module1/UIStyle/ArtUIStyleManager+UIStyleModule1.swift
@@ -0,0 +1,21 @@
+//
+//  ArtUIStyleManager+UIStyleModule1.swift
+//  ChangeTheme_Swift
+//
+//  Created by weijingyun on 2017/9/4.
+//  Copyright © 2017年 weijingyun. All rights reserved.
+//
+
+import UIKit
+
+extension ArtUIStyleManager {
+    
+    func getStyleName_Module1() -> String! {
+        return "Module1Style.plist"
+    }
+    
+    func getHotReloaderStylePath_Module1() -> String! {
+        return "/App/Module1/UIStyle/Module1Style.plist";
+    }
+    
+}
